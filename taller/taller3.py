9da6bef6aa2017304a9703425dbef9342188524f
santiagojaramillo12345 <santiagojaramillochaverra@gmail.com>
2023-07-29 15:02:32 -0500

    punto 3 realizado pendiente punto 4

--- taller/taller3.py
+++ taller/taller3.py
@@ -4,10 +4,27 @@ mostrar que un número fue escrito 2 veces, que todos son iguales o por el contr
 todos son diferentes.
 """
 
-numero1 =int (input ("escribe un número entero"))
-numero2 =int (input ("escribe un número entero"))
-numero3 =int (input ("escribe un número entero"))
-numero4 =int (input ("escribe un número entero"))
+numero1 =int (input ("escribe un número entero "))
+numero2 =int (input ("escribe un número entero "))
+numero3 =int (input ("escribe un número entero "))
+numero4 =int (input ("escribe un número entero "))
 
-if numero2 == numero1  and numero2== numero3 and numero2 == numero4:
-    print(f"el")
+if numero1 == numero2  and numero2== numero3 and numero3 == numero4:
+    print(f"todos los numeros son igules")
+elif numero1==numero2 and numero3==numero4:
+    print(f"el primer y segundo numero estan repetidos y son {numero1} y el tercer y cuarto tambien estan repetidos y el numero es {numero3} ")
+elif numero1==numero2 :
+    print(f"el primer y segundo numero estan repetidos y son {numero1}  ")
+elif numero3==numero4:
+     print(f"el tercer y cuarto numero estan repetidos y son {numero3}  ")
+elif numero1==numero4:
+ print(f"el primer y cuarto numero estan repetidos y son {numero1} ")
+elif numero1==numero3:
+ print(f"el primer y el tercero numero estan repetidos y son {numero1} ")
+
+elif numero2==numero3:
+     print(f"el segundo y tercero numero estan repetidos y son {numero2} ")
+elif numero2==numero4:
+     print(f"el segundo y el cuarto numero estan repetidos y son {numero2} ")
+else:
+    print("todos los nuemeros son diferentes y son {numero1}, {numero2}, {numero3}, {numero4} ")
---
+++ taller/taller4.py
@@ -0,0 +1,10 @@
+"""
+
+Realizar un programa que muestre un menú con opciones de colores primario
+(Amarillo, azul, rojo, blanco y negro), el usuario debe escoger 2 colores de los
+ofrecidos en el menú y con una secuencia de if y elif evaluar las posibles
+combinaciones generadas con los colores primarios. Si el usuario escogió
+colores que no generan otro al ser combinados debe mostrar un mensaje de
+“opciones no validas”
+
+"""
